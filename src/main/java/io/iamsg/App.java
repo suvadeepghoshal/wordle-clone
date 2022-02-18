fe45ca79f5e36fd7ff8d282a5e703694ee2737e8
Suvadeep Ghoshal <ghoshalsuvadeep594@gmail.com>
2022-02-18 22:15:49 +0530

    (updt) main menu

--- src/main/java/io/iamsg/App.java
+++ src/main/java/io/iamsg/App.java
@@ -5,8 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
- * Hello world!
- *
+ * @author SG
  */
 public class App {
     public static void main(String[] args) {
@@ -19,5 +18,22 @@ public class App {
 
     private static void menu() {
         // TODO: Create default display method
+        System.out.println();
+        System.out.println(
+                "***************************************************************************************************");
+        System.out.println("Welcome to WRODLE");
+        System.out.println("Guess the WORDLE in six tries");
+        System.out.println(
+                "After each try the color of the letter will change to show how close your guess was to the word");
+        System.out.println(
+                "***************************************************************************************************");
+        System.out.println("__RULES__");
+        System.out.println(">> Each word has to be a valid five letter english word");
+        System.out.println(
+                ">> If the letter is present in the WORDLE and postion of the letter is not correct then letter will turn YELLOW");
+        System.out.println(
+                ">> If the letter and the position of the letter matches with that of the WORDLE then the letter will turn GREEN");
+        System.out.println(">> If the letter is not present in the WORDLE then it will remain without colour");
+        System.out.println();
     }
 }
