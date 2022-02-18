af48b90f1d5cb3052962ffd1a2b0f383b1d7d350
Suvadeep Ghoshal <ghoshalsuvadeep594@gmail.com>
2022-02-18 22:04:50 +0530

    (updt) main class skeleton

--- src/main/java/io/iamsg/App.java
+++ src/main/java/io/iamsg/App.java
@@ -1,13 +1,23 @@
 package io.iamsg;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            menu();
+        } catch (IOException ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+
+    private static void menu() {
+        // TODO: Create default display method
     }
 }
