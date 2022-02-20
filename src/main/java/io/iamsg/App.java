9e59c57db06d675fb338fc7daa55a3bc7cbd9ba3
Suvadeep Ghoshal <ghoshalsuvadeep594@gmail.com>
2022-02-20 10:46:35 +0530

    (del) removing unwanted imports from the class

--- src/main/java/io/iamsg/App.java
+++ src/main/java/io/iamsg/App.java
@@ -7,7 +7,6 @@ import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
